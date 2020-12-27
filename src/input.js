e282573d83a558790df329dc971059bc0fa1ec64
Gabriel <gabrielvt6@gmail.com>
2020-12-26 17:19:56 -0800

    Added Input class

---
+++ src/input.js
@@ -0,0 +1,81 @@
+import * as keyCodes from './keyCodes';
+
+export default class Input {
+  constructor() {
+    this.keyDownHandler = this.keyDownHandler.bind(this);
+    this.keyUpHandler = this.keyUpHandler.bind(this);
+    this.pressed = {
+      right: false,
+      left: false,
+      up: false,
+      down: false,
+      rotateLeft: false,
+      rotateRight: false,
+      space: false,
+      pause: false
+    }
+  }
+
+  attachHandlers() {
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+  }
+
+  keyDownHandler(e) {
+    switch (e.keyCode) {
+      case keyCodes.A:
+        this.pressed.left = true;
+        break;
+      case keyCodes.LEFT:
+        this.pressed.rotateLeft = true;
+        break;
+      case keyCodes.W:
+        this.pressed.up = true;
+        break;
+      case keyCodes.D:
+        this.pressed.right = true;
+        break;
+      case keyCodes.RIGHT:
+        this.pressed.rotateRight = true;
+        break;
+      case keyCodes.S:
+        this.pressed.down = true;
+        break;
+      case keyCodes.P:
+        this.pressed.pase = true;
+        break;
+      case keyCodes.SPACE:
+        e.preventDefault();
+        this.pressed.space = true;
+        break;
+    }
+  }
+
+  keyUpHandler(e) {
+    switch (e.keyCode) {
+      case keyCodes.A:
+        this.pressed.left = false;
+        break;
+      case keyCodes.LEFT:
+        this.pressed.rotateLeft = false;
+      case keyCodes.W:
+        this.pressed.up = false;
+        break;
+      case keyCodes.D:
+        this.pressed.right = false;
+        break;
+      case keyCodes.RIGHT:
+        this.pressed.rotateRight = false;
+        break;
+      case keyCodes.S:
+        this.pressed.down = false;
+        break;
+      case keyCodes.SPACE:
+        this.pressed.space = false;
+        break;
+      case keyCodes.P:
+        this.pressed.pause = false;
+        break;
+    }
+  }
+}
---
+++ src/keyCodes.js
@@ -0,0 +1,8 @@
+export const W = 87;
+export const A = 65;
+export const S = 83;
+export const D = 68;
+export const LEFT = 37;
+export const RIGHT = 39;
+export const SPACE = 32;
+export const P = 80;
