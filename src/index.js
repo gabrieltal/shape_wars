1d98ba1cbb6174ca65261d073b965538f177b0b3
Gabriel <gabrielvt6@gmail.com>
2020-12-26 23:01:42 -0800

    Added helpful code comments on index js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import Sound from './sound.js';
 import Input from './input.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Set up Canvas and User Input
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   const input = new Input();
 
+  // Set up audio and sound controls
   const sound = new Sound();
-  const game = new Game(ctx, input, sound);
+  sound.attachHandlers();
 
+  // Set up game and add event listener to start game
+  const game = new Game(ctx, input, sound);
   document.getElementById('startGame').addEventListener('click', function() {
     game.start();
   });
-
 });
