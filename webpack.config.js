eec5a8e2a4c57e6eec1aa9635720d219d91e7404
Gabriel <gabrielvt6@gmail.com>
2020-12-26 17:02:05 -0800

    Set up webpack with style, css and babel loaders

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        "test": /\.js$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "@babel/preset-env",
+            ]
+          }
+        }
+      },
+    ]
+  }
+};
